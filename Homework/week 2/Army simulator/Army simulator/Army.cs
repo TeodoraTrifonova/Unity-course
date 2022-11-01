753afc792835afe441dadfc671ac747654d62373
TeodoraTrifonova <92271887+TeodoraTrifonova@users.noreply.github.com>
2022-11-01 14:20:37 +0200

    .

---
+++ Homework/week 2/Army simulator/Army simulator/Army.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Army_simulator
+{
+    public class Army
+    {
+
+    }
+}
---
+++ Homework/week 2/Army simulator/Army simulator/UnitActions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Army_simulator
+{
+    public interface UnitActions
+    {
+        public void AbilityInfo(Unit unit);
+
+    }
+}
